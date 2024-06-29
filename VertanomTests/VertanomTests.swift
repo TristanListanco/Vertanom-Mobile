91b7f91eada579ee5aaec82bca5a42fb2d98c3c5
Tristan Listanco <tannyencina@gmail.com>
2024-06-29 22:33:25 +0800

    Initial Commit

---
+++ VertanomTests/VertanomTests.swift
@@ -0,0 +1,17 @@
+//
+//  VertanomTests.swift
+//  VertanomTests
+//
+//  Created by Tristan Listanco on 6/29/24.
+//
+
+import Testing
+@testable import Vertanom
+
+struct VertanomTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

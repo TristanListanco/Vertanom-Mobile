c828f16626fe03fa2d53b8e4b51e458f5a422d74
Tristan Listanco <tannyencina@gmail.com>
2024-07-06 10:17:15 +0800

    Added WidgetKit

---
+++ Vertanom-WidgetKit/Vertanom_WidgetKitBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Vertanom_WidgetKitBundle.swift
+//  Vertanom-WidgetKit
+//
+//  Created by Tristan Listanco on 7/6/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Vertanom_WidgetKitBundle: WidgetBundle {
+    var body: some Widget {
+        Vertanom_WidgetKit()
+    }
+}

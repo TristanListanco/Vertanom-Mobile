87872cbd8068ae7821532abd17e5892b8b9c2560
Tristan Listanco <tannyencina@gmail.com>
2024-07-08 10:18:26 +0800

    Added TipKit UI (iOS 17+)

---
+++ Vertanom/View/DeviceDetail/AddDevice.swift
@@ -0,0 +1,18 @@
+//
+//  AddDevice.swift
+//  Vertanom
+//
+//  Created by Tristan Listanco on 7/7/24.
+//
+
+import SwiftUI
+
+struct AddDevice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddDevice()
+}
